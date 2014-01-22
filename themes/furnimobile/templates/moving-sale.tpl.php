ccc62792594c9d9c5a949a35925001e70eb77c32
Emil Akhmirov <akhmirem@gmail.com>
2014-01-22 00:36:27 -0800

    Added moving sale template files

---
+++ themes/furnimobile/templates/moving-sale.tpl.php
@@ -0,0 +1,53 @@
+<?php $files_dir = base_path() . variable_get('file_public_path', conf_path() . '/files'); ?>
+
+<article id="moving-sale" style="font-size:1.5em; line-height:1.5em">
+	<p>Dear Preferred Customer,</p>
+	
+	<p><span style="font-weight:700">Sacramento’s two premiere destinations for contemporary
+	furniture, Furnitalia and Copenhagen, are
+	going through exciting renovations and will be
+	exchanging showrooms. Together we are running a</span>
+	<span style="font-weight:700;" class="furn-red">Fantastic Moving Sale!</span></p>
+	
+	<p>Much of our combined inventory must be sold off the floor
+	immediately! Floor models from both Natuzzi and Stressless
+	brands, are priced at a fraction of the retail cost. With over
+	30,000 square feet of premium home furnishings made in
+	Italy, Norway, and Denmark there is a great deal for every
+	room in your house!</p>
+	
+	<p class="furn-ucase furn-red" style="text-align:center;font-size:1.8em;font-weight:700">SALE STARTS<br/> <span style="top: .3em;position: relative;">JANUARY 28, 2014!</span></p>
+	
+	<p style="text-align:center;"><span class="furn-red" style="font-size:1.5em;">You don't want to miss this <br/>once-in-a-lifetime sale!</span><br/><span style="font-size:1.2em">Hurry for the biggest selection of the <br/> lowest prices we have ever offered!</span> </p>
+	
+	<p style="font-size:2em;text-align:center;font-weight:700" class="furn-red furn-ucase">WE MOVE, YOU SAVE!</p>
+	
+	<p style="text-align:center"><img src="<?php print $files_dir;?>/promo/moving_sale/435x200_NatuzziOnlineAd.jpg"  alt="Moving sale at Furnitalia"/></p>
+	
+	<hr/>
+	
+	<section class="store-info clearfix">
+		<h2 class="field-label furn-e3">Furnitalia Sacramento</h2>
+		<div class="map"><iframe width="99%" height="223" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/ms?msa=0&amp;msid=211487104845968262296.0004968861c94e20ef05a&amp;hl=en&amp;ie=UTF8&amp;t=m&amp;ll=38.664335,-121.349316&amp;spn=0.014945,0.025234&amp;z=14&amp;output=embed"></iframe></div>
+		<div class="store-descr">		
+			
+			
+			<p class="info info-left"><span class="store-address">
+				5270 Auburn Boulevard <br/>
+				Sacramento, California 95841</span><br/>
+				<span class="location-label">Located in the Copenhagen Plaza,
+				 on the corner of Madison Avenue and
+				Auburn Boulevard next to the U-haul Rentals.</span>
+			</p>
+			
+			<p class="info info-right">
+				Phone: <br/><a href="tel:+19164840333" class="phone furn-grey">(916) 484–0333</a> <br/>
+				<span class="store-subtext">Hours: <span class="working-hours"><br/>Monday – Sunday <br/>
+				10:00 am – 6:00 pm</span></span>
+				<a href="https://maps.google.com/maps?saddr=&daddr=Furnitalia,+5270+Auburn+Boulevard,+Sacramento,+CA+95841&z=11" target="_blank" class="directions-link furn-button-text">GET DIRECTIONS</a>
+				<small>(Provided by Google Maps)</small>
+			</p>
+			
+		</div>
+	</section>
+</article>
---
+++ themes/furnitheme/templates/moving-sale.tpl.php
@@ -0,0 +1,51 @@
+<?php $files_dir = base_path() . variable_get('file_public_path', conf_path() . '/files'); ?>
+
+<article id="moving-sale" style="font-size:1.5em; line-height:1.5em">
+	<p>Dear Preferred Customer,</p>
+	
+	<p><span style="font-weight:700">Sacramento’s two premiere destinations for contemporary
+	furniture, Furnitalia and Copenhagen, are
+	going through exciting renovations and will be
+	exchanging showrooms. Together we are running a</span>
+	<span style="font-weight:700;" class="furn-red">Fantastic Moving Sale!</span></p>
+	
+	<p>Much of our combined inventory must be sold off the floor
+	immediately! Floor models from both Natuzzi and Stressless
+	brands, are priced at a fraction of the retail cost. With over
+	30,000 square feet of premium home furnishings made in
+	Italy, Norway, and Denmark there is a great deal for every
+	room in your house!</p>
+	
+	<p class="furn-ucase furn-red" style="text-align:center;font-size:1.8em;font-weight:700">SALE STARTS<br/> <span style="top: .3em;position: relative;">JANUARY 28, 2014!</span></p>
+	
+	<p style="text-align:center;"><span class="furn-red" style="font-size:1.5em;">You don't want to miss this <br/>once-in-a-lifetime sale!</span><br/><span style="font-size:1.2em">Hurry for the biggest selection of the <br/> lowest prices we have ever offered!</span> </p>
+	
+	<p style="font-size:2em;text-align:center;font-weight:700" class="furn-red furn-ucase">WE MOVE, YOU SAVE!</p>
+	
+	<p style="text-align:center"><img src="<?php print $files_dir;?>/promo/moving_sale/435x200_NatuzziOnlineAd.jpg"  alt="Moving sale at Furnitalia"/></p>
+	
+	<hr class="grey"/>
+		
+	<section class="store-info clearfix">
+		<div class="map"><iframe width="295" height="223" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/ms?msa=0&amp;msid=211487104845968262296.0004968861c94e20ef05a&amp;hl=en&amp;ie=UTF8&amp;t=m&amp;ll=38.664335,-121.349316&amp;spn=0.014945,0.025234&amp;z=14&amp;output=embed"></iframe></div>
+		<div class="store-descr">
+			<h2 class="field-label">Furnitalia Sacramento</h2>
+			<span class="location-label">Located in the Copenhagen Plaza</span>
+			
+			<p class="info">
+			5270 Auburn Boulevard <br/>
+			Sacramento, California 95841<br/>
+			– located on the corner of Madison Avenue and
+			Auburn Boulevard next to the U-haul Rentals.
+			</p>
+			
+			<p class="info">
+			Phone: <a href="tel:+19164840333" class="phone">(916) 484–0333</a> <br/>
+			Hours: Monday – Sunday <br/>
+			<span class="hours-indent">10:00 am – 6:00 pm</span>
+			</p>
+			<a href="https://maps.google.com/maps?saddr=&daddr=Furnitalia,+5270+Auburn+Boulevard,+Sacramento,+CA+95841&z=11" target="_blank" class="directions-link">GET DIRECTIONS</a>
+			<small>(Provided by Google Maps)</small>
+		</div>
+	</section>
+</article>
