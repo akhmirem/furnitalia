b69870edd11817569ef4f51afad2a3e62d8965eb
Emil Akhmirov <akhmirem@gmail.com>
2014-02-13 00:54:02 -0800

    Added ABC marketing script tag

--- themes/furnimobile/templates/html.tpl.php
+++ themes/furnimobile/templates/html.tpl.php
@@ -25,6 +25,9 @@
   </div>
   <?php print $page_top; ?>
   <?php print $page; ?>
+
+  <script language='JavaScript1.1' src='http://pixel.mathtag.com/event/js?mt_id=411755&mt_adid=119881&v1=&v2=&v3=&s1=&s2=&s3='></script>
+
   <?php print $page_bottom; ?>
 </body>
 </html>
