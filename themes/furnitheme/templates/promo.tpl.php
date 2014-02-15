5505abdd8aa40f8ef2a35798afbd4a1aceb7db5f
Emil Akhmirov <akhmirem@gmail.com>
2014-02-14 21:14:00 -0800

    banner img updated

--- themes/furnitheme/templates/promo.tpl.php
+++ themes/furnitheme/templates/promo.tpl.php
@@ -1,6 +1,12 @@
 <h3>Moving SALE</h3>
 <hr/>
-<a href="/moving-sale?utm_source=main&utm_medium=banner&utm_campaign=moving-sale"><img src="<?php print base_path(); ?>sites/default/files/promo/moving_sale/moving-sale-755x100.gif" alt="Moving Sale"/></a>
+<a href="/moving-sale?utm_source=main&utm_medium=banner&utm_campaign=moving-sale">
+	<?php if (drupal_match_path(current_path(), '<front>')) : ?>
+		<img src="<?php print base_path(); ?>sites/default/files/promo/moving_sale/755x100_MainImageGraphic_01.jpg" alt="Moving Sale"/>
+	<?php else: ?>
+		<img src="<?php print base_path(); ?>sites/default/files/promo/moving_sale/moving-sale-755x100.gif" alt="Moving Sale"/>
+	<?php endif; ?>
+</a>
 <!-- <h4></h4> -->
 <!--<p>
 Enjoy the Holiday Sale till the end of the year. In stores and online.
