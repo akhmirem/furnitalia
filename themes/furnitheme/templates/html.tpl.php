287e8c961d29083e5768f9a55aea424b7353d287
Emil Akhmirov <akhmirem@gmail.com>
2014-02-24 19:23:32 -0800

    Moved marketing (pixel) script declaration to the very bottom

--- themes/furnitheme/templates/html.tpl.php
+++ themes/furnitheme/templates/html.tpl.php
@@ -108,8 +108,9 @@
   <?php print $page_top; ?>
   <?php print $page; ?>
 
-  <script language='JavaScript1.1' src='http://pixel.mathtag.com/event/js?mt_id=411755&mt_adid=119881&v1=&v2=&v3=&s1=&s2=&s3='></script>
-
   <?php print $page_bottom; ?>
+
+  <!-- load marketing scripts -->
+  <script language='JavaScript1.1' src='http://pixel.mathtag.com/event/js?mt_id=411755&mt_adid=119881&v1=&v2=&v3=&s1=&s2=&s3='></script>
 </body>
 </html>
