12daa7917ed3ea8ec72cbc8ef55e82381fd72dd7
Emil Akhmirov <akhmirem@gmail.com>
2014-06-04 18:00:22 -0700

    Update to modules Jun 1, 2014

--- themes/furnitheme/templates/promo.tpl.php
+++ themes/furnitheme/templates/promo.tpl.php
@@ -1,4 +1,4 @@
-<h3>As Low as We Can Go: Memorial Day SALE</h3>
+<h3>As Low as We Can Go: Floor Models on SALE</h3>
 <hr/>
 <a href="/living?availability=In+stock">
 		<img src="<?php print base_path(); ?>sites/default/files/promo/pre_memorial_sale/755x100_MainImageGraphic.jpg" alt="Memorial Day Sale"/>
