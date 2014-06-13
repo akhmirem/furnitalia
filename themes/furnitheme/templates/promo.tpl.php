32a1a863b3a256febd05e6668985ed17db7cacff
Emil Akhmirov <akhmirem@gmail.com>
2014-06-12 23:20:42 -0700

    Fathers Day Sale graphics update

--- themes/furnitheme/templates/promo.tpl.php
+++ themes/furnitheme/templates/promo.tpl.php
@@ -1,7 +1,7 @@
-<h3>As Low as We Can Go: Floor Models on SALE</h3>
+<h3>Happy Fathers Day - Italian Way!</h3>
 <hr/>
 <a href="/living?availability=In+stock">
-		<img src="<?php print base_path(); ?>sites/default/files/promo/pre_memorial_sale/755x100_MainImageGraphic.jpg" alt="Memorial Day Sale"/>
+		<img src="<?php print base_path(); ?>sites/default/files/promo/fathers_day/755x100_MainImageGraphic.jpg" alt="Fathers Day Sale"/>
 </a>
 <!--<p>
 Enjoy the Holiday Sale till the end of the year. In stores and online.
