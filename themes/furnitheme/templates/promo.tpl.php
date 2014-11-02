c0633d7097d61480b7b5837f7f2bbc2c8d6bea13
Emil Akhmirov <akhmirem@gmail.com>
2014-11-02 13:54:24 -0800

    Returned fall 2014 banners

--- themes/furnitheme/templates/promo.tpl.php
+++ themes/furnitheme/templates/promo.tpl.php
@@ -1,7 +1,7 @@
-<h3>October Clearance Event...</h3>
+<h3>Just in Time for Holidays...</h3>
 <hr/>
 <a href="/living?availability=in_stock">
-		<img src="<?php print base_path(); ?>sites/default/files/promo/oct2014/755x100_MainImageGraphic.jpg" alt="See how much you can save just in time for holidays!"/>
+		<img src="<?php print base_path(); ?>sites/default/files/promo/fall2014/755x100_MainImageGraphic.jpg" alt="See how much you can save just in time for holidays!"/>
 </a>
 <!--<p>
 Enjoy the Holiday Sale till the end of the year. In stores and online.
